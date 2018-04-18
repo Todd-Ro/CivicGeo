1365e0911682cb339140718bd836fd0d4e11abb0
Todd Romkema <todd.romkema@gmail.com>
2018-04-17 20:23:32 -0500

    First Commit

---
+++ CivicGeoApplication.java
@@ -0,0 +1,12 @@
+package org.esq.toddrom.CivicGeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicGeoApplication.class, args);
+	}
+}
