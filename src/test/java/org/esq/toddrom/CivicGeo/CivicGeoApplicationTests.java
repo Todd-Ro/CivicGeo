117a75f56e70a67971bac06e1ec86f5b2872fd39
Todd Romkema <todd.romkema@gmail.com>
2018-05-01 20:35:20 -0500

    Initial commit for reinitialization

---
+++ src/test/java/org/esq/toddrom/CivicGeo/CivicGeoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.esq.toddrom.CivicGeo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CivicGeoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
