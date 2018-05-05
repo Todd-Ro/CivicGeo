15dae5c6acb648fef543b10ec6231d2720895b2b
Todd Romkema <todd.romkema@gmail.com>
2018-05-05 15:09:33 -0500

    Got Admin Type Enum Working

--- Models/Forms/User.java
+++ Models/Forms/User.java
@@ -1,13 +1,23 @@
 package org.esq.toddrom.CivicGeo.Models.Forms;
 
+import org.hibernate.validator.constraints.Email;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Entity
 public class User {
+
+    static final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+    private Date date;
+    private String dateString;
 
     @Id
     @GeneratedValue
@@ -16,20 +26,27 @@ public class User {
     @NotNull
     private String name;
 
-    @NotNull
+    @Email
     private String email;
 
     private UserAdminType adminType;
 
-
+    @NotNull
+    @Size(min=6, message = "Password should have at least 6 characters")
+    private String password;
 
     public User() { }
 
     public User(String name, String email) {
         this.name = name;
         this.email=email;
+        this.date = new Date();
+        this.dateString = dateFormat.format(this.date);
     }
 
+
+
+    //Getters and Setters
 
     public int getId() {
         return id;
@@ -67,4 +84,27 @@ public class User {
         this.adminType = UserAdminType.valueOf(adminLevelString);
     }
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDateString() {
+        return dateString;
+    }
+
+    public void setDateString(String dateString) {
+        this.dateString = dateString;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
